2f1a9fa0d3cdad4b84eafaa629225906ca278682
Varun007-program <varuntripathy04@gmail.com>
2023-03-04 15:34:53 +0530

    Created Loading screen using PyQt5

---
+++ splash_screen_work_using_pyqt5/circular_progress_folder/circular_progress.py
@@ -0,0 +1,95 @@
+# from PySide6.QtCore import *
+# from PySide6.QtGui import *
+# from PySide6.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class CircularProgress(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        # custome parameters
+        self.value = 0
+        self.width = 200
+        self.height = 200
+        self.progress_width = 10
+        self.progress_rounded_cap = True
+        self.progress_color = 0xff79c6
+        self.max_value = 100
+        #  text
+        self.enable_text = True
+        self.font_family = "Segoe UI"
+        self.font_size = 12
+        self.suffix = "%"
+        self.text_color = 0xff79c6
+        # bg
+        self.enable_bg = True
+        self.bg_color = 0x44475a
+
+        # self.enable_shadow = True
+
+        # set default size without layout
+        self.resize(self.width, self.height)
+
+    def add_shadow(self, enable):
+        if enable:
+            self.shadow = QGraphicsDropShadowEffect(self)
+            self.shadow.setBlurRadius(15)
+            self.shadow.setXOffset(0)
+            self.shadow.setYOffset(0)
+            self.shadow.setColor(QColor(0,0,0,80))
+            self.setGraphicsEffect(self.shadow) 
+
+    def set_value(self, value):
+        self.value = value
+        self.repaint() # render progress bar after change value
+
+    #  paint event (design for circular progress)
+    def paintEvent(self, event):
+        width = self.width - self.progress_width
+        height = self.height - self.progress_width
+        margin = self.progress_width / 2
+        value = self.value * 360 / self.max_value
+
+        # painter
+        paint = QPainter()
+        paint.begin(self)
+        paint.setRenderHint(QPainter.Antialiasing)
+        paint.setFont(QFont(self.font_family, self.font_size))
+
+        # rectangle
+        rect = QRect(0, 0, self.width, self.height)
+        paint.setPen(Qt.NoPen)
+        paint.drawRect(rect)
+
+        # pen
+        pen = QPen()
+        pen.setWidth(self.progress_width)
+
+        if self.progress_rounded_cap:
+            pen.setCapStyle(Qt.RoundCap)
+
+        # enable bg
+        if self.enable_bg:
+            pen.setColor(QColor(self.bg_color))
+            paint.setPen(pen)
+            paint.drawArc(margin, margin, width, height, 0, 360 * 16)
+
+        # create arc
+        pen.setColor(QColor(self.progress_color))
+        paint.setPen(pen)        
+        paint.drawArc(margin, margin, width, height, -90 * 16, -value * 16)
+
+        #  create text
+        if self.enable_text:
+            pen.setColor(QColor(self.text_color))
+            paint.setPen(pen)
+            paint.drawText(rect, Qt.AlignCenter, f'{self.value}{self.suffix}')
+
+        pen.setColor(QColor(self.text_color))
+        paint.setPen(pen)
+        paint.drawText(rect, Qt.AlignCenter, f'{self.value}{self.suffix}')     
+
+        paint.end()
+
